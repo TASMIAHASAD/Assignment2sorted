a957990316637a150fb39aac9c06fc2cf8d7f6e7
Syeda Tasmiah Asad <44003304+TASMIAHASAD@users.noreply.github.com>
2018-12-25 00:41:19 -0800

    Add files via upload
    driver file added

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "Customer.h"
+#include <stdio.h>
+#include<string>
+
+
+using namespace std;
+int main()
+{
+    Customer <int> Tasmiah;
+    Tasmiah.GetPersonName();
+    Tasmiah.GetNationalID();
+    for(int index=0;index<15;index++{
+     Tasmiah.GetPhoneNumber();
+    }
+
+    for(int index=0;index<5;index++{
+    Tasmiah.GetOperators();
+    }
+
+    Customer <int> Shimaly;
+    Shimaly.GetPersonName();
+    Shimaly.GetNationalID();
+    for(int index=0;index<15;index++{
+     Shimaly.GetPhoneNumber();
+    }
+
+    for(int index=0;index<5;index++{
+    Shimaly.GetOperators();
+    }
+
+
+}
+
